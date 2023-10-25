ef97f8edf7420f13eee8b76591819e252a206aa5
EliasE83 <mayorelias83@gmail.com>
2023-10-25 17:28:28 -0600

    Practica 6 completa

--- ProyectoLaravel/app/Http/Controllers/DiarioController.php
+++ ProyectoLaravel/app/Http/Controllers/DiarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\validadorFormRecuerdos; //Instruccion para importar el validador
 
 class DiarioController extends Controller //Clase que hereda todo lo que necesita de Controller
 {
@@ -19,17 +20,30 @@ class DiarioController extends Controller //Clase que hereda todo lo que necesit
         return view('recuerdos');
     }
 
-    public function guardarRecuerdo(Request $req){
-        //return $req->all();
-        //Codigo php para imprimir en pantalla
-        echo "<p>";
-        echo $req->ip();
-        echo " - ";
-        echo $req->path(); //Ruta
-        echo " - ";
-        echo $req->method();  
-        echo " - ";
-        echo $req->input('txtTitulo'); 
-        "</p>";
+    public function guardarRecuerdo(validadorFormRecuerdos $req){
+
+        /* $validated = $req->validate([ //La validacion es un arreglo
+            // La primera parte de la validacion es el nombre del input
+            // La segunda parte es la regla de validacion
+            'txtTitulo' => 'required|max:25',
+            'txtRecuerdo' => 'required|min:4',
+        ]); */
+        
+        /* Redirigimos al formulario con un mensaje con la funcion with donde el primer parametro es
+        un identificador y el segundo parametro es el mensaje */
+
+        return redirect('/formulario')->with('confirmacion','Recuerdo guardado con exito');
+        
+        // Codigo php para imprimir en pantalla
+        // echo "<p>";
+        // echo $req->ip();
+        // echo " - ";
+        // echo $req->path(); //Ruta
+        // echo " - ";
+        // echo $req->method();  
+        // echo " - ";
+        // echo $req->input('txtTitulo'); 
+        // "</p>";
+
     }
 }
--- ProyectoLaravel/resources/views/formulario.blade.php
+++ ProyectoLaravel/resources/views/formulario.blade.php
@@ -8,6 +8,27 @@
 
 <div class="container mt-5 col-md-6">
     
+  @if (session()->has('confirmacion'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>{{ session('confirmacion')}}</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  @endif
+
+  {{-- @if ($errors->any())
+
+    @foreach ($errors->all() as $error)
+
+    <div class="alert alert-dark alert-dismissible fade show" role="alert">
+      <strong>{{ $error }}</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
+    @endforeach
+    
+  @endif --}}
+
+
         <div class="card ">
 
             <div class="card-header fs-4 fw-medium text-danger text-center ">
@@ -21,13 +42,19 @@
 
                     <div class="mb-3">
                       <label class="form-label">Titulo: </label>
-                      <input type="text" name="txtTitulo" class="form-control" required>
+                      <input type="text" name="txtTitulo" class="form-control" value="{{ old('txtTitulo')}}" >
+
+                      <p class="text-danger fw-bold">{{ $errors->first('txtTitulo') }}</p>
+
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label">Recuerdo: </label>
-                        <input type="text" name="txtRecuerdo" class="form-control" required>
-                      </div>                   
+                        <input type="text" name="txtRecuerdo" class="form-control" value="{{ old('txtRecuerdo')}}">
+
+                        <p class="text-danger fw-bold">{{ $errors->first('txtRecuerdo') }}</p>
+
+                    </div>                   
             </div>
         
             <div class="card-footer text-body-secondary">
